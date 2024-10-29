7779ab0cb3746af3ef2001f20fda233e87789555
Calvinsd <calvinsd1011@gmail.com>
2024-10-29 23:21:38 +0530

    chore: some updates

--- node/express-api/src/index.js
+++ node/express-api/src/index.js
@@ -1,5 +1,19 @@
 const express = require('express')
-const app = express()
+const app = express();
+const pg = require('pg');
+require('dotenv').config();
+
+const client  = new pg.Client({
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_ROOT_PASSWORD,
+    host: 'pg-service',
+    port: process.env.POSTGRESDB_LOCAL_PORT,
+    database: process.env.POSTGRES_DATABASE,
+    statement_timeout: 10000,
+    query_timeout: 10000,
+});
+
+client.connect().then(()=> console.log('connected to postgres db'));
 
 app.get('/',(req,res)=> {
 console.log('received request')
